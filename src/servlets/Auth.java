985155934ed490abddc1c7b6af413ac6222cfaba
Ferreira <takyto@laposte.net>
2014-10-11 17:25:16 +0200

    premeire partie non finis pb redirection

---
+++ src/servlets/Auth.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Auth
+ */
+
+public class Auth extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Auth() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		response.setContentType("text/plain");
+		
+		
+		if(name.contentEquals(password)){
+			response.getWriter().println("Hello Mr "+name);
+		}else{
+			 response.sendRedirect("/login.html");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
