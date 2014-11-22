2865661b8cba31d0d9d51dafeee21d4545853734
AdrieanKhisbe <adriean.khisbe@live.fr>
2014-11-22 22:54:27 +0100

    fix post dep update HibernateUtils (was old one...) + restore servlet-api jar

--- src/util/HibernateUtil.java
+++ src/util/HibernateUtil.java
@@ -7,32 +7,32 @@ import javax.management.ObjectName;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.jmx.StatisticsService;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory;
-	static {
-		try {
-			sessionFactory = new Configuration().configure()
-					.buildSessionFactory();
-			// FIXME Deprecated????
-
-			// for JConsole (Hibernate monitoring)
-			StatisticsService statsMBean = new StatisticsService();
-			statsMBean.setSessionFactory(sessionFactory);
-			statsMBean.setStatisticsEnabled(true);
-
-			MBeanServer mBeanServer = ManagementFactory
-					.getPlatformMBeanServer();
-			mBeanServer.registerMBean(statsMBean, new ObjectName(
-					"Hibernate:application=Statistics"));
-
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+	
+	private static SessionFactory sessionFactory= buildSessionFactory();
+	private static ServiceRegistry serviceRegistry;
+  
+    private static SessionFactory buildSessionFactory() {
+        try {
+        	// Create the SessionFactory from hibernate.cfg.xml
+        	Configuration configuration = new Configuration().configure();
+     
+            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                    configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            return sessionFactory;
+           
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
