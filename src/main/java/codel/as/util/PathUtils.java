82f7f133cac06bb5c52b2208669c809a8577c337
AdrieanKhisbe <adriean.khisbe@live.fr>
2014-11-24 21:38:35 +0100

    setting up a filter: logic to be made!!

--- src/main/java/codel/as/util/PathUtils.java
+++ src/main/java/codel/as/util/PathUtils.java
@@ -8,12 +8,12 @@ package codel.as.util;
  */
 public class PathUtils {
 
-	public static final String LOGIN_SERVLET = "login",
-			LOGIN_PAGE = "login.jsp", ADD_SERVLET = "contact/add",
-			ADD_PAGE = "addContact.jsp", DELETE_SERVLET = "contact/delete",
-			DELETE_PAGE = "deleteContact.jsp",
-			UPDATE_SERVLET = "contact/update",
-			UPDATE_PAGE = "updateContact.jsp",
-			SEARCH_SERVLET = "contact/update",
-			SEARCH_PAGE = "updateContact.jsp", ACCUEIL = "accueil.jsp";
+	public static final String LOGIN_SERVLET = "/login",
+			LOGIN_PAGE = "/login.jsp", ADD_SERVLET = "contact/add",
+			ADD_PAGE = "/addContact.jsp", DELETE_SERVLET = "contact/delete",
+			DELETE_PAGE = "/deleteContact.jsp",
+			UPDATE_SERVLET = "/contact/update",
+			UPDATE_PAGE = "/updateContact.jsp",
+			SEARCH_SERVLET = "/contact/update",
+			SEARCH_PAGE = "/updateContact.jsp", ACCUEIL = "accueil.jsp";
 }
